18d87335c6672fae3bad8271d8c5f8b963332af6
raibarra04 <ribarra764@gmail.com>
2020-12-22 17:23:54 -0800

    Worked on adding recipe

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = '05445896-78ed-4910-8704-0c93a5ac2412';
+export const MODAL_CLOSE_SEC = 2.5;
